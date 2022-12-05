7962c5e8109d91010548b41dae8a3dc892996810
Brent McConnell <emcconne@gmail.com>
2022-12-05 15:31:47 -0500

    added main

--- main.tf
+++ main.tf
@@ -70,69 +70,69 @@ resource "azurerm_public_ip" "pub_ip" {
   tags = local.tags
 }
 
-resource "azurerm_application_gateway" "network" {
-  name                = var.app_gateway_name
-  location            = local.location
-  resource_group_name = local.resource_group 
+# resource "azurerm_application_gateway" "network" {
+#   name                = var.app_gateway_name
+#   location            = local.location
+#   resource_group_name = local.resource_group 
 
-  sku {
-    name     = var.app_gateway_sku
-    tier     = "Standard_v2"
-    capacity = 2
-  }
+#   sku {
+#     name     = var.app_gateway_sku
+#     tier     = "Standard_v2"
+#     capacity = 2
+#   }
 
-  gateway_ip_configuration {
-    name      = "appGatewayIpConfig"
-    subnet_id = data.azurerm_subnet.appgwsubnet.id
-  }
+#   gateway_ip_configuration {
+#     name      = "appGatewayIpConfig"
+#     subnet_id = data.azurerm_subnet.appgwsubnet.id
+#   }
 
-  frontend_port {
-    name = local.frontend_port_name
-    port = 80
-  }
+#   frontend_port {
+#     name = local.frontend_port_name
+#     port = 80
+#   }
 
-  frontend_port {
-    name = "httpsPort"
-    port = 443
-  }
+#   frontend_port {
+#     name = "httpsPort"
+#     port = 443
+#   }
 
-  frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
-    public_ip_address_id = azurerm_public_ip.pub_ip.id
-  }
+#   frontend_ip_configuration {
+#     name                 = local.frontend_ip_configuration_name
+#     public_ip_address_id = azurerm_public_ip.pub_ip.id
+#   }
 
-  backend_address_pool {
-    name = local.backend_address_pool_name
-  }
+#   backend_address_pool {
+#     name = local.backend_address_pool_name
+#   }
 
-  backend_http_settings {
-    name                  = local.http_setting_name
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 1
-  }
+#   backend_http_settings {
+#     name                  = local.http_setting_name
+#     cookie_based_affinity = "Disabled"
+#     port                  = 80
+#     protocol              = "Http"
+#     request_timeout       = 1
+#   }
 
-  http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
-    protocol                       = "Http"
-  }
+#   http_listener {
+#     name                           = local.listener_name
+#     frontend_ip_configuration_name = local.frontend_ip_configuration_name
+#     frontend_port_name             = local.frontend_port_name
+#     protocol                       = "Http"
+#   }
 
-  request_routing_rule {
-    name                       = local.request_routing_rule_name
-    rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
-    priority                   = 10
-  }
+#   request_routing_rule {
+#     name                       = local.request_routing_rule_name
+#     rule_type                  = "Basic"
+#     http_listener_name         = local.listener_name
+#     backend_address_pool_name  = local.backend_address_pool_name
+#     backend_http_settings_name = local.http_setting_name
+#     priority                   = 10
+#   }
 
-  tags = local.tags
+#   tags = local.tags
 
-  depends_on = [azurerm_virtual_network.vnet, azurerm_public_ip.pub_ip]
-}
+#   depends_on = [azurerm_virtual_network.vnet, azurerm_public_ip.pub_ip]
+# }
 
 resource "azurerm_role_assignment" "ra1" {
   scope                = data.azurerm_subnet.kubesubnet.id
@@ -141,18 +141,17 @@ resource "azurerm_role_assignment" "ra1" {
   depends_on           = [azurerm_virtual_network.vnet]
 }
 
-resource "azurerm_role_assignment" "ra3" {
-  scope                = azurerm_application_gateway.network.id
-  role_definition_name = "Contributor"
-  principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
-  depends_on           = [azurerm_application_gateway.network]
-}
+# resource "azurerm_role_assignment" "ra3" {
+#   scope                = azurerm_application_gateway.network.id
+#   role_definition_name = "Contributor"
+#   principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+#   depends_on           = [azurerm_application_gateway.network]
+# }
 
 resource "azurerm_role_assignment" "ra4" {
   scope                = data.azurerm_resource_group.project-rg.id
   role_definition_name = "Reader"
   principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
-  depends_on           = [azurerm_application_gateway.network]
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
@@ -191,6 +190,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     service_cidr       = var.aks_service_cidr
   }
 
-  depends_on = [azurerm_virtual_network.vnet, azurerm_application_gateway.network]
+  depends_on = [azurerm_virtual_network.vnet]
+  #depends_on = [azurerm_virtual_network.vnet, azurerm_application_gateway.network]
   tags       = local.tags
 }
